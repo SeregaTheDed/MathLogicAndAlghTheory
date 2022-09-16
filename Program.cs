d60299a5c1eaf76a6f7ccaf9e4759466a3557d42
SeregaTheDed <shtanko33333@mail.ru>
2022-09-16 14:14:06 +0300

    add gitignore

--- Program.cs
+++ Program.cs
@@ -4,10 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //string inputLine = "((-x1|x2)>x3)".ToLower();
-            //string inputLine = "(-(p>(q&p))>(p|r))";
-            //string inputLine = "(a>b)";
             Task.Task1();
+            Console.ReadKey();
         }
     }
 }
